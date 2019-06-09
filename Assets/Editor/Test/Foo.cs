ba533592b5810840b2e39a2ebda02da7310daa1d
willdavis <willdavis84@gmail.com>
2019-06-08 21:35:40 -0700

    added basic structure for unit testing

---
+++ Assets/Editor/Test/Foo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Test
+{
+    public class Foo
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void FooSimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator FooWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
