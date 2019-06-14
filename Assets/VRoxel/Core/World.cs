6d9fc110bded985056e7e75c535951d9e01da52d
willdavis <willdavis84@gmail.com>
2019-06-13 22:53:14 -0700

    added World prefab

--- Assets/VRoxel/Core/World.cs
+++ Assets/VRoxel/Core/World.cs
@@ -5,26 +5,6 @@ using UnityEngine;
 public class World : MonoBehaviour
 {
     /// <summary>
-    /// The voxel dimensions for the world
-    /// </summary>
-    public Vector3Int size = new Vector3Int(16, 16, 16);
-
-    /// <summary>
-    /// The number of voxels in a chunk
-    /// </summary>
-    public Vector3Int chunkSize = new Vector3Int(16, 16, 16);
-
-    /// <summary>
-    /// The number of chunks in a region
-    /// </summary>
-    public Vector3Int regionSize = new Vector3Int(1, 1, 1);
-
-    /// <summary>
-    /// The scale factor for the world
-    /// </summary>
-    public float scale = 1.0f;
-
-    /// <summary>
     /// Unity prefab for a Chunk
     /// </summary>
     public Chunk chunk;
@@ -33,6 +13,26 @@ public class World : MonoBehaviour
     /// Unity Material to use as the texture atlas
     /// </summary>
     public Material material;
+
+    /// <summary>
+    /// The scale factor for the world
+    /// </summary>
+    public float scale = 1f;
+
+    /// <summary>
+    /// The voxel dimensions for the world
+    /// </summary>
+    public Vector3Int size = new Vector3Int(256, 256, 256);
+
+    /// <summary>
+    /// The number of voxels in a chunk
+    /// </summary>
+    public Vector3Int chunkSize = new Vector3Int(32, 32, 32);
+
+    /// <summary>
+    /// The number of chunks in a region
+    /// </summary>
+    public Vector3Int regionSize = new Vector3Int(8, 8, 8);
 
     void OnDrawGizmos()
     {
