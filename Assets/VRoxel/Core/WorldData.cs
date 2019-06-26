6af51bac79bf246f87fce81dc9d87c51ae1a6de5
willdavis <willdavis84@gmail.com>
2019-06-26 00:52:03 -0700

    refactored WorldData to cache the world size

--- Assets/VRoxel/Core/WorldData.cs
+++ Assets/VRoxel/Core/WorldData.cs
@@ -7,9 +7,11 @@ public class WorldData
 {
     private byte[,,] _cache;
     private Vector3 _center;
+    private Vector3Int _size;
 
     public WorldData(Vector3Int size)
     {
+        _size = size;
         _cache = new byte[size.x, size.y, size.z];
         _center = new Vector3(size.x / 2f, size.y / 2f, size.z / 2f);
     }
@@ -25,9 +27,9 @@ public class WorldData
     /// <param name="point">The point to compare</param>
     public bool Contains(Vector3Int point)
     {
-        if (point.x < 0 || point.x >= _cache.GetLength(0)) { return false; }
-        if (point.y < 0 || point.y >= _cache.GetLength(1)) { return false; }
-        if (point.z < 0 || point.z >= _cache.GetLength(2)) { return false; }
+        if (point.x < 0 || point.x >= _size.x) { return false; }
+        if (point.y < 0 || point.y >= _size.y) { return false; }
+        if (point.z < 0 || point.z >= _size.z) { return false; }
         return true;
     }
 
