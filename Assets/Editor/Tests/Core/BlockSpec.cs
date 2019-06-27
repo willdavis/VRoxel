6d05fa79e9d19947b78ef636df68f8f19c4ae004
willdavis <willdavis84@gmail.com>
2019-06-27 00:42:23 -0700

    added Block class

---
+++ Assets/Editor/Tests/Core/BlockSpec.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class BlockSpec
+    {
+        [Test]
+        public void HasName()
+        {
+            Block block = new Block();
+            block.name = "test";
+            Assert.AreEqual("test", block.name);
+        }
+
+        [Test]
+        public void HasTextures()
+        {
+            Block block = new Block();
+            block.textures = new Dictionary<Cube.Direction, Vector2>();
+            block.textures.Add(Cube.Direction.Top, Vector2.one);
+
+            Assert.AreEqual(Vector2.one, block.textures[Cube.Direction.Top]);
+        }
+    }
+}
---
+++ Assets/VRoxel/Core/Block.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Blocks contain information about voxels in the world
+/// </summary>
+public class Block
+{
+    public string name;
+    public Dictionary<Cube.Direction, Vector2> textures;
+}
