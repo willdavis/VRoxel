b4a4601bf91c357a3aa48fb6f3e633dc42c8ca39
willdavis <willdavis84@gmail.com>
2019-06-27 20:04:44 -0700

    added BlockManager class

---
+++ Assets/Editor/Tests/Core/BlockManagerSpec.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class BlockManagerSpec
+    {
+        [Test]
+        public void HasBlocks()
+        {
+            byte index = 0;
+            Block block = new Block();
+            BlockManager manager = new BlockManager();
+
+            manager.blocks.Add(index, block);
+            Assert.AreSame(block, manager.blocks[index]);
+        }
+
+        [Test]
+        public void HasTextures()
+        {
+            Material material = new Material(Shader.Find("Specular"));
+            BlockManager manager = new BlockManager();
+
+            manager.textures.material = material;
+            manager.textures.size = 10f;
+
+            Assert.AreSame(material, manager.textures.material);
+            Assert.AreEqual(10f, manager.textures.size);
+        }
+    }
+}
---
+++ Assets/VRoxel/Core/BlockManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Manages and configures all blocks in a world
+/// </summary>
+public class BlockManager
+{
+    /// <summary>
+    /// The texture data common to all blocks
+    /// </summary>
+    public class TextureAtlas
+    {
+        /// <summary>
+        /// The spacing between block textures in the atlas
+        /// </summary>
+        public float size;
+
+        /// <summary>
+        /// The source material for the texture atlas
+        /// </summary>
+        public Material material;
+    }
+
+    /// <summary>
+    /// The texture data common to all blocks
+    /// </summary>
+    public TextureAtlas textures;
+    
+    /// <summary>
+    /// Connects each block to a byte index used in the VoxelGrid
+    /// </summary>
+    public Dictionary<byte, Block> blocks;
+
+    public BlockManager()
+    {
+        blocks = new Dictionary<byte, Block>();
+        textures = new TextureAtlas();
+    }
+}
