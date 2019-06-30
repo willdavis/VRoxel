d8acd62591102908f82c4bce5bb61adfb88f0fb3
willdavis <willdavis84@gmail.com>
2019-06-30 00:01:27 -0700

    added test texture atlas material

--- Assets/Editor/Tests/Core/ChunkSpec.cs
+++ Assets/Editor/Tests/Core/ChunkSpec.cs
@@ -31,17 +31,17 @@ namespace Tests
         [UnityTest]
         public IEnumerator CanGenerateMesh()
         {
-            Material material = new Material(Shader.Find("Specular"));
+            Material material = AssetDatabase.LoadAssetAtPath<Material>("Assets/Editor/Materials/TextureAtlas.mat");
+            World prefab_world = AssetDatabase.LoadAssetAtPath<World>("Assets/VRoxel/Prefabs/World.prefab");
+            Chunk prefab_chunk = AssetDatabase.LoadAssetAtPath<Chunk>("Assets/VRoxel/Prefabs/Chunk.prefab");
 
             // setup a world
-            World prefab_world = AssetDatabase.LoadAssetAtPath<World>("Assets/VRoxel/Prefabs/World.prefab");
             World world = UnityEngine.Object.Instantiate(prefab_world, Vector3.zero, Quaternion.identity) as World;
             world.chunkSize = new Vector3Int(2,2,2);
             world.size = new Vector3Int(2,2,2);
             world.Initialize(); // needed so that the VoxelGrid is initialized
 
             // setup a chunk
-            Chunk prefab_chunk = AssetDatabase.LoadAssetAtPath<Chunk>("Assets/VRoxel/Prefabs/Chunk.prefab");
             Chunk chunk = UnityEngine.Object.Instantiate(prefab_chunk, Vector3.zero, Quaternion.identity) as Chunk;
             chunk.transform.position += new Vector3(-0.5f, -0.5f, -0.5f); // align the chunk with the world size (2,2,2)
 
@@ -55,7 +55,7 @@ namespace Tests
             Block block = new Block();
             world.blocks.library.Add(1, block);
             world.blocks.texture.material = material;
-            world.blocks.texture.size = 0.5f;
+            world.blocks.texture.size = 1f;
 
             // setup textures for the block
             block.textures.Add(Cube.Direction.Top, Vector2.zero);
@@ -70,7 +70,7 @@ namespace Tests
             chunk.GenerateMesh();
 
             yield return null; // skip a frame to render the chunk
-            //System.Threading.Thread.Sleep(1000); // sleep so we can see it
+            System.Threading.Thread.Sleep(2000); // sleep so we can see it
 
             // check the generated mesh
             Mesh mesh = chunk.GetComponent<MeshFilter>().sharedMesh;
