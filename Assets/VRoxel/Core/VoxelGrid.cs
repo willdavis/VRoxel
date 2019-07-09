373d20cf5f35899fd61da85718ddac9d6d34d59a
willdavis <willdavis84@gmail.com>
2019-07-09 00:24:12 -0700

    added more ways to set voxel data

--- Assets/VRoxel/Core/VoxelGrid.cs
+++ Assets/VRoxel/Core/VoxelGrid.cs
@@ -34,6 +34,14 @@ public class VoxelGrid
     }
 
     /// <summary>
+    /// Unsafely get a block type from the voxel grid cache
+    /// </summary>
+    /// <param name="x">the X coordinate</param>
+    /// <param name="y">the Y coordinate</param>
+    /// <param name="z">the Z coordinate</param>
+    public byte Get(int x, int y, int z) { return _cache[x, y, z]; }
+
+    /// <summary>
     /// Safely get a block type from the voxel grid cache
     /// </summary>
     /// <param name="point">A point inside the world</param>
@@ -44,18 +52,19 @@ public class VoxelGrid
     }
 
     /// <summary>
-    /// Unsafely get a block type from the voxel grid cache
+    /// Unsafely set a block index in the voxel grid cache
     /// </summary>
     /// <param name="x">the X coordinate</param>
     /// <param name="y">the Y coordinate</param>
     /// <param name="z">the Z coordinate</param>
-    public byte Get(int x, int y, int z) { return _cache[x, y, z]; }
+    /// <param name="block">The block index to set</param>
+    public void Set(int x, int y, int z, byte block) { _cache[x, y, z] = block; }
 
     /// <summary>
-    /// Safely set a block index in the voxel grid cache
+    /// Safely set a block in the voxel grid cache
     /// </summary>
     /// <param name="point">A point inside the voxel grid</param>
-    /// <param name="block">The block index to be set</param>
+    /// <param name="block">The block index to set</param>
     public void Set(Vector3Int point, byte block)
     {
         if (!Contains(point)) { return; }
@@ -63,11 +72,67 @@ public class VoxelGrid
     }
 
     /// <summary>
-    /// Unsafely set a block index in the voxel grid cache
+    /// Safely set a range of blocks in the voxel grid
     /// </summary>
-    /// <param name="x">the X coordinate</param>
-    /// <param name="y">the Y coordinate</param>
-    /// <param name="z">the Z coordinate</param>
-    /// <param name="block">The block index to be set</param>
-    public void Set(int x, int y, int z, byte block) { _cache[x, y, z] = block; }
+    /// <param name="start">A point in the voxel grid</param>
+    /// <param name="end">A point in the voxel grid</param>
+    /// <param name="block">The block index to set</param>
+    public void Set(Vector3Int start, Vector3Int end, byte block)
+    {
+        if (!Contains(start)) { return; }
+        if (!Contains(end)) { return; }
+
+        Vector3Int delta = Vector3Int.zero;
+        delta.x = Mathf.Abs(end.x - start.x) + 1;
+        delta.y = Mathf.Abs(end.y - start.y) + 1;
+        delta.z = Mathf.Abs(end.z - start.z) + 1;
+
+        Vector3Int min = Vector3Int.zero;
+        min.x = Mathf.Min(start.x, end.x);
+        min.y = Mathf.Min(start.y, end.y);
+        min.z = Mathf.Min(start.z, end.z);
+
+        for (int x = min.x; x < min.x + delta.x; x++)
+        {
+            for (int z = min.z; z < min.z + delta.z; z++)
+            {
+                for (int y = min.y; y < min.y + delta.y; y++)
+                {
+                    Set(x, y, z, block);
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Safely set a Moore neighborhood of blocks in the voxel grid
+    /// </summary>
+    /// <param name="start">A point in the voxel grid</param>
+    /// <param name="range">The Moore neighborhood range</param>
+    /// <param name="block">The block index to set</param>
+    /// range = 0, 1^3 block neighborhood
+    /// range = 1, 3^3 block neighborhood
+    /// range = 2, 5^3 block neighborhood
+    /// range = 3, 7^3 block neighborhood
+    public void Set(Vector3Int point, int range, byte block)
+    {
+        if (!Contains(point)) { return; }
+
+        int neighborhood = 2 * range + 1;
+        Vector3Int offset = Vector3Int.zero;
+
+        for (int i = 0; i < neighborhood; i++) // x-axis
+        {
+            offset.x = point.x + i - range;
+            for (int j = 0; j < neighborhood; j++) // z-axis
+            {
+                offset.z = point.z + j - range;
+                for (int k = 0; k < neighborhood; k++) // y-axis
+                {
+                    offset.y = point.y + k - range;
+                    Set(offset, block);
+                }
+            }
+        }
+    }
 }
