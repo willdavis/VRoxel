51e5c9e44283256d7b82244f5c307fd2bdf16f45
willdavis <willdavis84@gmail.com>
2019-07-18 23:23:54 -0700

    added a path to the navagent

--- Assets/VRoxel/Navigation/NavAgent.cs
+++ Assets/VRoxel/Navigation/NavAgent.cs
@@ -4,22 +4,40 @@ using UnityEngine;
 
 public class NavAgent : MonoBehaviour
 {
-    public Vector3 destination;
+    private Vector3 _destination;
+    private Stack<Vector3> _path = new Stack<Vector3>();
+
     public float range = 0.5f;
     public float speed = 5.0f;
 
+    public Vector3 destination
+    {
+        get { return _destination; }
+        set { _destination = value; _path.Clear(); }
+    }
+
     void Start()
     {
-        destination = transform.position;
+        _destination = transform.position;
     }
 
     void Update()
     {
         float distance = Vector3.Distance(transform.position, destination);
-        if (distance > range)
-        {
-            float step = speed * Time.deltaTime;
-            transform.position = Vector3.MoveTowards(transform.position, destination, step);
-        }
+        if (distance <= range * range) { return; } // agent is at the destination
+        if (_path.Count == 0) { BuildPath(); } // build a path to the destination
+
+        // move towards the next waypoint in the path
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, _path.Peek(), step);
+
+        distance = Vector3.Distance(transform.position, _path.Peek());
+        if (distance <= range * range) { _path.Pop(); } // agent is at the waypoint
+    }
+
+    void BuildPath()
+    {
+        _path.Push(_destination);
+        _path.Push(Vector3.Lerp(transform.position, _destination, 0.5f) + (Vector3.up * 2.5f));
     }
 }
