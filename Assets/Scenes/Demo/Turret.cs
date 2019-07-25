1af5b2df0eb9c69931c7c88a19c51b3c24a88aea
willdavis <willdavis84@gmail.com>
2019-07-24 23:25:59 -0700

    updated demo turrets to shoot at the nearest target

--- Assets/Scenes/Demo/Turret.cs
+++ Assets/Scenes/Demo/Turret.cs
@@ -5,11 +5,48 @@ using UnityEngine;
 public class Turret : MonoBehaviour
 {
     [Header("Settings")]
-    public Transform fireFrom;
-    public LineRenderer bullet;
+    public Transform fireFrom;      // the point to shoot from
+    public LineRenderer bullet;     // a lazer beam
 
     [Header("Runtime")]
-    public List<NavAgent> targets;
-    public float range;         // the distance the turret can shoot
-    public float dps;           // damage per second
+    public List<NavAgent> targets;  // all potential targets in the world
+    public float range;             // the distance the turret can shoot
+
+    void Update()
+    {
+        bullet.SetPosition(0, fireFrom.transform.position); // reset the lazer
+        bullet.SetPosition(1, fireFrom.transform.position); // reset the lazer
+
+        if (targets.Count == 0) { return; } // no targets available
+
+        NavAgent target = FindTarget();
+        if (target == null) { return; }     // no targets in range
+
+        bullet.SetPosition(0, fireFrom.transform.position); // set the lazer on the target
+        bullet.SetPosition(1, target.transform.position);   // set the lazer on the target
+
+        targets.Remove(target);         // kill the target instantly
+        Destroy(target.gameObject);     // and remove the gameObject
+    }
+
+    NavAgent FindTarget()
+    {
+        NavAgent target = null;
+        float shortest = float.MaxValue;
+        for (int i = 0; i < targets.Count; i++)
+        {
+            NavAgent agent = targets[i];
+            float dist = Vector3.Distance(
+                agent.transform.position,
+                fireFrom.transform.position
+            );
+
+            if (dist <= range && dist <= shortest)
+            {
+                shortest = dist;
+                target = agent;
+            }
+        }
+        return target;
+    }
 }
