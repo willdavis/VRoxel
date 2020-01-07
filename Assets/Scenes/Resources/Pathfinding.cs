d84b6e000f34c21630454e8ac459a8c66958228d
willdavis <willdavis84@gmail.com>
2020-01-07 13:54:58 -0800

    created Pathfinding demo script and moved some prefabs to the Resources folder

---
+++ Assets/Scenes/Resources/Pathfinding.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using VRoxel.Core;
+
+public class Pathfinding : MonoBehaviour
+{
+    World _world;
+
+    [Header("Setting Goals")]
+    public Vector2Int destination;
+    public GameObject goalPost;
+
+    void Awake()
+    {
+        _world = GetComponent<World>();
+    }
+}
