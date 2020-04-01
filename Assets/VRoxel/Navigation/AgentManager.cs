2ababcc5631e488b46aadaff6c2c616592966e69
willdavis <1072649+willdavis@users.noreply.github.com>
2020-03-31 22:08:51 -0700

    #19 removed AgentManager.Spawn function

--- Assets/VRoxel/Navigation/AgentManager.cs
+++ Assets/VRoxel/Navigation/AgentManager.cs
@@ -2,18 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-using VRoxel.Core;
 
 namespace VRoxel.Navigation
 {
     public class AgentManager
     {
-        private World _world;
         private List<NavAgent> _agents;
 
-        public AgentManager(World world)
+        public AgentManager()
         {
-            _world = world;
             _agents = new List<NavAgent>();
         }
 
@@ -21,20 +18,6 @@ namespace VRoxel.Navigation
         /// Returns a list of all managed agents
         /// </summary>
         public List<NavAgent> all { get { return _agents; } }
-
-        /// <summary>
-        /// Adds a new NavAgent (NPC) to the World
-        /// </summary>
-        public NavAgent Spawn(NavAgent prefab, Vector3 position)
-        {
-            Quaternion rotation = _world.transform.rotation;
-            NavAgent agent = UnityEngine.Object.Instantiate(prefab, position, rotation) as NavAgent;
-            agent.transform.localScale = Vector3.one * _world.scale;
-            agent.transform.parent = _world.transform;
-
-            _agents.Add(agent);
-            return agent;
-        }
 
         /// <summary>
         /// Update each agents position in the world
