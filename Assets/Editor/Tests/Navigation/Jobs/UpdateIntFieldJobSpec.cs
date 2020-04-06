7c6de116e9420a3ead1b9bf16a9790acf4e4b189
willdavis <1072649+willdavis@users.noreply.github.com>
2020-04-06 14:22:18 -0700

    #35 created job to update the integration field

---
+++ Assets/Editor/Tests/Navigation/Jobs/UpdateIntFieldJobSpec.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using Unity.Jobs;
+using UnityEngine.Jobs;
+using Unity.Collections;
+
+using VRoxel.Navigation;
+
+using NUnit.Framework;
+using UnityEngine.TestTools;
+
+namespace NavigationSpecs
+{
+    public class UpdateIntFieldJobSpec
+    {
+        [Test]
+        public void UpdatesTheIntegrationField()
+        {
+            
+        }
+    }
+}
---
+++ Assets/VRoxel/Navigation/Jobs/UpdateIntFieldJob.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using Unity.Collections;
+using UnityEngine.Jobs;
+using Unity.Jobs;
+
+namespace VRoxel.Navigation
+{
+    /// <summary>
+    /// Updates the integration field
+    /// </summary>
+    public struct UpdateIntFieldJob : IJob
+    {
+        [ReadOnly]
+        public NativeArray<byte> costField;
+
+        [WriteOnly]
+        public NativeArray<ushort> intField;
+
+        public void Execute()
+        {
+            
+        }
+    }
+}
