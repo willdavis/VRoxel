9a14454c85ebf626ddcd1f7b80a95e9a73ce257b
willdavis <1072649+willdavis@users.noreply.github.com>
2020-04-05 23:13:20 -0700

    #34 added helper function to test if a point is outside the flow field

--- Assets/VRoxel/Navigation/Jobs/UpdateFlowFieldJob.cs
+++ Assets/VRoxel/Navigation/Jobs/UpdateFlowFieldJob.cs
@@ -7,6 +7,9 @@ namespace VRoxel.Navigation
 {
     public struct UpdateFlowFieldJob : IJobParallelFor
     {
+        /// <summary>
+        /// the size of the flow field
+        /// </summary>
         public Vector3Int size;
 
         [ReadOnly]
@@ -24,9 +27,9 @@ namespace VRoxel.Navigation
         }
 
         /// <summary>
-        /// Calculate a 1D array index from a Vector3Int position
+        /// Calculate a 1D array index from a Vector3Int point
         /// </summary>
-        /// <param name="point">A point in the voxel grid</param>
+        /// <param name="point">A point in the flow field</param>
         public int Flatten(Vector3Int point)
         {
             /// A[x,y,z] = A[ x * height * depth + y * depth + z ]
@@ -42,6 +45,18 @@ namespace VRoxel.Navigation
             int y = (index - x * size.y * size.z) / size.z;
             int z = index - x * size.y * size.z - y * size.z;
             return new Vector3Int(x,y,z);
+        }
+
+        /// <summary>
+        /// Test if a point is inside the flow field
+        /// </summary>
+        /// <param name="point">A point in the flow field</param>
+        public bool OutOfBounds(Vector3Int point)
+        {
+            if (point.x < 0 || point.x >= size.x) { return true; }
+            if (point.y < 0 || point.y >= size.y) { return true; }
+            if (point.z < 0 || point.z >= size.z) { return true; }
+            return false;
         }
     }
 }
