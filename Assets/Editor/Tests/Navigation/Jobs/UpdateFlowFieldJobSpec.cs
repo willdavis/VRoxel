5e399e1a27575c41ff085ac6c923a229d07b83d2
willdavis <1072649+willdavis@users.noreply.github.com>
2020-04-05 21:58:59 -0700

    #34 created job to update the flow field asynchronously

---
+++ Assets/Editor/Tests/Navigation/Jobs/UpdateFlowFieldJobSpec.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using Unity.Jobs;
+using UnityEngine.Jobs;
+using Unity.Collections;
+
+using VRoxel.Core;
+using VRoxel.Navigation;
+
+using NUnit.Framework;
+using UnityEngine.TestTools;
+
+namespace NavigationSpecs
+{
+    public class UpdateFlowFieldJobSpec
+    {
+        [Test]
+        public void UpdatesTheFlowField()
+        {
+            Vector3Int size = new Vector3Int(1, 1, 2);
+            int flatSize = size.x * size.y * size.z;
+
+            NativeArray<int> intField = new NativeArray<int>(flatSize, Allocator.Persistent);
+            NativeArray<byte> flowField = new NativeArray<byte>(flatSize, Allocator.Persistent);
+            NativeArray<Vector3Int> flowDirections = new NativeArray<Vector3Int>(27, Allocator.Persistent);
+
+            for (int i = 0; i < 27; i++)
+                flowDirections[i] = Direction3Int.Directions[i];
+
+            UpdateFlowFieldJob job = new UpdateFlowFieldJob()
+            {
+                flowDirections = flowDirections,
+                flowField = flowField,
+                intField = intField
+            };
+
+            JobHandle handle = job.Schedule(flatSize, 1);
+            handle.Complete();
+
+            Assert.AreEqual((byte)Direction3Int.Name.Zero, flowField[0]);
+            Assert.AreEqual((byte)Direction3Int.Name.Zero, flowField[1]);
+
+            intField.Dispose();
+            flowField.Dispose();
+            flowDirections.Dispose();
+        }
+    }
+}
---
+++ Assets/VRoxel/Navigation/Jobs/UpdateFlowFieldJob.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using Unity.Collections;
+using UnityEngine.Jobs;
+using Unity.Jobs;
+
+namespace VRoxel.Navigation
+{
+    public struct UpdateFlowFieldJob : IJobParallelFor
+    {
+        [ReadOnly]
+        public NativeArray<int> intField;
+
+        [ReadOnly]
+        public NativeArray<Vector3Int> flowDirections;
+
+        [WriteOnly]
+        public NativeArray<byte> flowField;
+
+        public void Execute(int i)
+        {
+            
+        }
+    }
+}
