2d4e6577e2edbe5a34b4f69262799daf26a35f13
willdavis <1072649+willdavis@users.noreply.github.com>
2020-04-06 19:25:21 -0700

    #35 construct the integration field using Dijkstra’s Algorithm

--- Assets/Editor/Tests/Navigation/Jobs/UpdateIntFieldJobSpec.cs
+++ Assets/Editor/Tests/Navigation/Jobs/UpdateIntFieldJobSpec.cs
@@ -7,6 +7,7 @@ using UnityEngine.Jobs;
 using Unity.Collections;
 
 using VRoxel.Navigation;
+using VRoxel.Core;
 
 using NUnit.Framework;
 using UnityEngine.TestTools;
@@ -18,7 +19,42 @@ namespace NavigationSpecs
         [Test]
         public void UpdatesTheIntegrationField()
         {
+            Vector3Int size = new Vector3Int(1, 1, 4);
+            int flatSize = size.x * size.y * size.z;
+
+            NativeArray<byte> costField = new NativeArray<byte>(flatSize, Allocator.Persistent);
+            NativeArray<ushort> intField = new NativeArray<ushort>(flatSize, Allocator.Persistent);
+            NativeArray<Vector3Int> directions = new NativeArray<Vector3Int>(27, Allocator.Persistent);
+
+            for (int i = 0; i < flatSize; i++)
+            {
+                costField[i] = 2;
+                intField[i] = ushort.MaxValue;
+            }
+
+            for (int i = 0; i < 27; i++)
+                directions[i] = Direction3Int.Directions[i];
             
+            UpdateIntFieldJob job = new UpdateIntFieldJob()
+            {
+                size = size,
+                goal = Vector3Int.zero,
+                directions = directions,
+                costField = costField,
+                intField = intField
+            };
+
+            JobHandle handle = job.Schedule();
+            handle.Complete();
+
+            Assert.AreEqual(0, intField[0]);
+            Assert.AreEqual(2, intField[1]);
+            Assert.AreEqual(4, intField[2]);
+            Assert.AreEqual(6, intField[3]);
+
+            costField.Dispose();
+            intField.Dispose();
+            directions.Dispose();
         }
     }
 }
--- Assets/VRoxel/Navigation/Jobs/UpdateIntFieldJob.cs
+++ Assets/VRoxel/Navigation/Jobs/UpdateIntFieldJob.cs
@@ -1,7 +1,9 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using Unity.Collections;
 using UnityEngine.Jobs;
 using Unity.Jobs;
+using Priority_Queue;
 
 namespace VRoxel.Navigation
 {
@@ -10,18 +12,57 @@ namespace VRoxel.Navigation
     /// </summary>
     public struct UpdateIntFieldJob : IJob
     {
+        /// <summary>
+        /// the size of the integration field
+        /// </summary>
         public Vector3Int size;
+
+        /// <summary>
+        /// the goal where all paths will direct to
+        /// </summary>
         public Vector3Int goal;
+
+        [ReadOnly]
+        public NativeArray<Vector3Int> directions;
 
         [ReadOnly]
         public NativeArray<byte> costField;
 
-        [WriteOnly]
         public NativeArray<ushort> intField;
 
         public void Execute()
         {
-            
+            if (OutOfBounds(goal)) { return; }
+
+            SimplePriorityQueue<Vector3Int, ushort> open = new SimplePriorityQueue<Vector3Int, ushort>();
+            open.Enqueue(goal, 0);          // queue the goal position as the first open node
+            intField[Flatten(goal)] = 0;    // set the goal position integration cost to zero
+
+            ushort cost;
+            int index, nextIndex;
+            Vector3Int position, nextPosition;
+
+            while (open.Count != 0)
+            {
+                position = open.Dequeue();
+                index = Flatten(position);
+
+                for (int i = 1; i < 27; i++)    // check neighbors
+                {
+                    nextPosition = position + directions[i];
+                    if (OutOfBounds(nextPosition)) { continue; }
+
+                    nextIndex = Flatten(nextPosition);
+                    if (costField[nextIndex] == 255) { continue; }  // check for obstructed node
+
+                    cost = Convert.ToUInt16(intField[index] + costField[nextIndex]);
+                    if (intField[nextIndex] == ushort.MaxValue || cost < intField[nextIndex])
+                    {
+                        intField[nextIndex] = cost;
+                        open.Enqueue(nextPosition, cost);
+                    }
+                }
+            }
         }
 
         /// <summary>
@@ -32,17 +73,6 @@ namespace VRoxel.Navigation
         {
             /// A[x,y,z] = A[ x * height * depth + y * depth + z ]
             return (point.x * size.y * size.z) + (point.y * size.z) + point.z;
-        }
-
-        /// <summary>
-        /// Calculate a Vector3Int point from an array index
-        /// </summary>
-        public Vector3Int UnFlatten(int index)
-        {
-            int x = index / (size.y * size.z);
-            int y = (index - x * size.y * size.z) / size.z;
-            int z = index - x * size.y * size.z - y * size.z;
-            return new Vector3Int(x,y,z);
         }
 
         /// <summary>
