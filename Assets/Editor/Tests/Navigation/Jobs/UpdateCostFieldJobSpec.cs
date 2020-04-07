8b7895f49a2acfa85632bbb4e4b294851842846a
willdavis <1072649+willdavis@users.noreply.github.com>
2020-04-06 19:58:19 -0700

    #36 create job to update the cost field

---
+++ Assets/Editor/Tests/Navigation/Jobs/UpdateCostFieldJobSpec.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using Unity.Jobs;
+using UnityEngine.Jobs;
+using Unity.Collections;
+
+using VRoxel.Navigation;
+using NUnit.Framework;
+
+namespace NavigationJobSpecs
+{
+    public class UpdateCostFieldJobSpec
+    {
+        [Test]
+        public void UpdatesTheField()
+        {
+            
+        }
+    }
+}
---
+++ Assets/VRoxel/Navigation/Jobs/UpdateCostFieldJob.cs
@@ -0,0 +1,19 @@
+﻿using Unity.Collections;
+using Unity.Jobs;
+
+namespace VRoxel.Navigation
+{
+    public struct UpdateCostFieldJob : IJobParallelFor
+    {
+        [ReadOnly]
+        public NativeArray<byte> voxels;
+
+        [WriteOnly]
+        public NativeArray<byte> costField;
+
+        public void Execute(int i)
+        {
+            
+        }
+    }
+}
