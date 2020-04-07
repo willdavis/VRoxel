e48163fe61f032b6007657157101a742c6e6f32f
willdavis <1072649+willdavis@users.noreply.github.com>
2020-04-06 19:43:25 -0700

    #40 created a job to reset the integration field

---
+++ Assets/Editor/Tests/Navigation/Jobs/ClearIntFieldJobSpec.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using Unity.Jobs;
+using UnityEngine.Jobs;
+using Unity.Collections;
+
+using VRoxel.Navigation;
+using NUnit.Framework;
+
+namespace NavigationJobSpecs
+{
+    public class ClearIntFieldJobSpec
+    {
+        [Test]
+        public void ClearsTheField()
+        {
+            Vector3Int size = new Vector3Int(2, 2, 2);
+            int flatSize = size.x * size.y * size.z;
+
+            NativeArray<ushort> intField = new NativeArray<ushort>(flatSize, Allocator.Persistent);
+
+            ClearIntFieldJob job = new ClearIntFieldJob()
+            {
+                intField = intField
+            };
+
+            JobHandle handle = job.Schedule(flatSize, 1);
+            handle.Complete();
+
+            for (int i = 0; i < flatSize; i++)
+            {
+                Assert.AreEqual(ushort.MaxValue, intField[i]);
+            }
+
+            intField.Dispose();
+        }
+    }
+}
---
+++ Assets/VRoxel/Navigation/Jobs/ClearIntFieldJob.cs
@@ -0,0 +1,19 @@
+﻿using Unity.Collections;
+using Unity.Jobs;
+
+namespace VRoxel.Navigation
+{
+    /// <summary>
+    /// Resets the integration field
+    /// </summary>
+    public struct ClearIntFieldJob : IJobParallelFor
+    {
+        [WriteOnly]
+        public NativeArray<ushort> intField;
+
+        public void Execute(int i)
+        {
+            intField[i] = ushort.MaxValue;
+        }
+    }
+}
