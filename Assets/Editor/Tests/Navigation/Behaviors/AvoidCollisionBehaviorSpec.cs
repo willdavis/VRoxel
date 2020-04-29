62883e13e0592e8581fb16de875b562e4a878f1c
willdavis <1072649+willdavis@users.noreply.github.com>
2020-04-28 19:50:52 -0700

    #82 adding tests for the AvoidCollisionBehavior

--- Assets/Editor/Tests/Navigation/Behaviors/AvoidCollisionBehaviorSpec.cs
+++ Assets/Editor/Tests/Navigation/Behaviors/AvoidCollisionBehaviorSpec.cs
@@ -4,25 +4,86 @@ using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 
-namespace Tests
+using Unity.Jobs;
+using UnityEngine.Jobs;
+using Unity.Collections;
+using Unity.Mathematics;
+
+using VRoxel.Navigation;
+
+namespace NavigationBehaviorSpecs
 {
     public class AvoidCollisionBehaviorSpec
     {
-        // A Test behaves as an ordinary method
         [Test]
-        public void AvoidCollisionBehaviorSpecSimplePasses()
+        public void CanCheckIfVectorIntersectsCircle()
         {
-            // Use the Assert class to test conditions
+            float radius = 1f;
+            float3 center = new float3(0, 0, 0);
+            float3 ahead  = new float3(0, 1, 0);
+            float3 ahead2 = ahead * 0.5f;
+
+            AvoidCollisionBehavior job = new AvoidCollisionBehavior() {  };
+
+            bool result;
+            result = job.IntersectsCircle(ahead, ahead2, center, radius);
+            Assert.AreEqual(true, result);      // inside
+
+            ahead += new float3(0,1f,0);
+            ahead2 = ahead * 0.5f;
+
+            result = job.IntersectsCircle(ahead, ahead2, center, radius);
+            Assert.AreEqual(true, result);      // on the edge
+
+            ahead += new float3(0,1f,0);
+            ahead2 = ahead * 0.5f;
+
+            result = job.IntersectsCircle(ahead, ahead2, center, radius);
+            Assert.AreEqual(false, result);     // outside
         }
 
-        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-        // `yield return null;` to skip a frame.
-        [UnityTest]
-        public IEnumerator AvoidCollisionBehaviorSpecWithEnumeratorPasses()
+        [Test]
+        public void CanCheckForMostThreateningAgent()
         {
-            // Use the Assert class to test conditions.
-            // Use yield to skip a frame.
-            yield return null;
+            bool result;
+            float3 target, best;
+            NativeArray<float3> positions = new NativeArray<float3>(1, Allocator.Persistent);
+            AvoidCollisionBehavior job = new AvoidCollisionBehavior()
+            {
+                position = positions
+            };
+
+            best = new float3(0, 1f, 0);
+            target  = new float3(0, 0.5f, 0);
+            result = job.MostThreatening(0, target, best);
+            Assert.AreEqual(true, result);
+
+            best = new float3(0, 0.5f, 0);
+            target  = new float3(0, 1f, 0);
+            result = job.MostThreatening(0, target, best);
+            Assert.AreEqual(false, result);
+
+            positions.Dispose();
         }
+
+        /*
+        [Test]
+        public void CanApplyAvoidanceForce()
+        {
+            
+        }
+
+        [Test]
+        public void CanDetectAnObstruction()
+        {
+            
+        }
+
+        [Test]
+        public void AvoidsNearbyAgents()
+        {
+            
+        }
+        */
     }
 }
