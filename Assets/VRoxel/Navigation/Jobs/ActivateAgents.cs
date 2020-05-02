46a0d4b2584dcd43685e1dcfbbd143157163e80f
willdavis <1072649+willdavis@users.noreply.github.com>
2020-05-01 18:22:00 -0700

    #87 created job to activate/deactivate agents

---
+++ Assets/VRoxel/Navigation/Jobs/ActivateAgents.cs
@@ -0,0 +1,23 @@
+﻿using Unity.Collections;
+using Unity.Jobs;
+using Unity.Burst;
+
+namespace VRoxel.Navigation
+{
+    /// <summary>
+    /// Changes an agents navigation activity
+    /// </summary>
+    [BurstCompile]
+    public struct ActivateAgents : IJobParallelFor
+    {
+        public bool status;
+
+        [ReadOnly]
+        public NativeSlice<bool> agents;
+
+        public void Execute(int i)
+        {
+            agents[i] = status;
+        }
+    }
+}
