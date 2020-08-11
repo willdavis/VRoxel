0cc17c02a71eccd88a69b54f889fef65a53c2a4c
willdavis <1072649+willdavis@users.noreply.github.com>
2020-08-10 20:06:10 -0700

    #94 created HeightMap

---
+++ Assets/Editor/Tests/Terrain/HeightMapSpec.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using VRoxel.Terrain;
+using UnityEngine;
+
+namespace TerrainSpecs
+{
+    public class HeightMapSpec
+    {
+        [Test]
+        public void CanReadHeightValues()
+        {
+            GameObject obj = new GameObject();
+            HeightMap map = obj.AddComponent<HeightMap>();
+
+            map.size = Vector2Int.one;
+            map.Initialize();
+
+            ushort max = ushort.MaxValue;
+            Assert.AreEqual(0, map.Read(0,0));
+            Assert.AreEqual(max, map.Read(1,1));
+
+            map.Dispose();
+        }
+
+        [Test]
+        public void CanCheckIfContainsPoint()
+        {
+            GameObject obj = new GameObject();
+            HeightMap map = obj.AddComponent<HeightMap>();
+
+            map.size = Vector2Int.one * 2;
+            Assert.AreEqual(true, map.Contains(0,0));
+            Assert.AreEqual(true, map.Contains(1,1));
+
+            Assert.AreEqual(false, map.Contains(2,2));
+            Assert.AreEqual(false, map.Contains(-1,-1));
+        }
+
+        [Test]
+        public void CanFlattenToIndex()
+        {
+            GameObject obj = new GameObject();
+            HeightMap map = obj.AddComponent<HeightMap>();
+
+            map.size = Vector2Int.one * 3;
+            Assert.AreEqual(0, map.Flatten(0,0));
+            Assert.AreEqual(1, map.Flatten(0,1));
+            Assert.AreEqual(2, map.Flatten(0,2));
+            Assert.AreEqual(3, map.Flatten(1,0));
+            Assert.AreEqual(4, map.Flatten(1,1));
+            Assert.AreEqual(5, map.Flatten(1,2));
+            Assert.AreEqual(6, map.Flatten(2,0));
+            Assert.AreEqual(7, map.Flatten(2,1));
+            Assert.AreEqual(8, map.Flatten(2,2));
+        }
+    }
+}
---
+++ Assets/VRoxel/Terrain/HeightMap.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+using VRoxel.Core;
+
+using Unity.Collections;
+using Unity.Mathematics;
+using Unity.Jobs;
+
+namespace VRoxel.Terrain
+{
+    /// <summary>
+    /// Represents a 2D array of height values for voxel terrain
+    /// </summary>
+    public class HeightMap : MonoBehaviour
+    {
+        /// <summary>
+        /// The reference to the voxel world
+        /// </summary>
+        [Tooltip("Can be left blank if the object has a World component")]
+        public World world;
+
+        /// <summary>
+        /// The maximum height value the height map can have
+        /// </summary>
+        public ushort maxHeight;
+
+        /// <summary>
+        /// The total (x,y) size of the height map
+        /// </summary>
+        public Vector2Int size;
+
+        /// <summary>
+        /// A 2D array of height values for the terrain
+        /// </summary>
+        NativeArray<ushort> m_data;
+
+        //-------------------------------------------------
+        #region Monobehaviors
+
+        protected virtual void Awake()
+        {
+            if (world == null)
+                world = GetComponent<World>();
+        }
+
+        protected virtual void Start()
+        {
+            Initialize();
+        }
+
+        protected virtual void OnDestroy()
+        {
+            Dispose();
+        }
+
+        #endregion
+        //-------------------------------------------------
+
+        //-------------------------------------------------
+        #region Public API
+
+        /// <summary>
+        /// Initializes the array of height map values
+        /// </summary>
+        public void Initialize()
+        {
+            m_data = new NativeArray<ushort>(
+                size.x * size.y, Allocator.Persistent
+            );
+        }
+
+        /// <summary>
+        /// Returns the height of the terrain at the point (x,y)
+        /// </summary>
+        public ushort Read(int x, int y)
+        {
+            if (!Contains(x,y))
+                return ushort.MaxValue;
+
+            return m_data[Flatten(x,y)];
+        }
+
+        /// <summary>
+        /// Converts a (x,y) position into an array index
+        /// </summary>
+        public int Flatten(int x, int y)
+        {
+            /// A[x,y] = A[x * height + y]
+            return x * size.y + y;
+        }
+
+        /// <summary>
+        /// Checks if a (x,y) position is inside the height map
+        /// </summary>
+        public bool Contains(int x, int y)
+        {
+            if (x < 0 || x >= size.x) { return false; }
+            if (y < 0 || y >= size.y) { return false; }
+            return true;
+        }
+
+        /// <summary>
+        /// Disposes all unmanaged memory
+        /// </summary>
+        public void Dispose()
+        {
+            m_data.Dispose();
+        }
+
+        #endregion
+        //-------------------------------------------------
+    }
+}
