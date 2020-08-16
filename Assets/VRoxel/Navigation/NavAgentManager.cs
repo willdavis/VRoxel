4c7df68c13d1ec6c8d8523a40397fd7ed3f680dc
willdavis <1072649+willdavis@users.noreply.github.com>
2020-08-15 18:51:24 -0700

    #108 created NavAgentManager Monobehavior

---
+++ Assets/VRoxel/Navigation/NavAgentManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using VRoxel.Navigation.Agents;
+using VRoxel.Navigation.Data;
+
+using UnityEngine;
+using UnityEngine.Jobs;
+using Unity.Collections;
+
+namespace VRoxel.Navigation
+{
+    /// <summary>
+    /// A component to help manage NavAgents in the scene
+    /// </summary>
+    public class NavAgentManager : MonoBehaviour
+    {
+        /// <summary>
+        /// The configurations for each type of NavAgent that should be managed
+        /// </summary>
+        public List<NavAgentConfiguration> configurations;
+
+
+
+        /// <summary>
+        /// The current kinematic properties of each managed agent
+        /// </summary>
+        protected NativeArray<AgentKinematics> m_agentKinematics;
+
+        /// <summary>
+        /// The async transform access to each managed agents transform
+        /// </summary>
+        protected TransformAccessArray m_transformAccess;
+
+
+
+        //-------------------------------------------------
+        #region Monobehaviors
+
+
+
+        #endregion
+        //-------------------------------------------------
+    }
+}
