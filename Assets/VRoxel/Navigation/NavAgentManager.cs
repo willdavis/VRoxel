b2a0dbb3e1482ed226aa95ab48bc44d9b2416caa
willdavis <1072649+willdavis@users.noreply.github.com>
2020-08-15 19:21:55 -0700

    #108 added public API to the NavAgentManager

--- Assets/VRoxel/Navigation/NavAgentManager.cs
+++ Assets/VRoxel/Navigation/NavAgentManager.cs
@@ -5,6 +5,7 @@ using VRoxel.Navigation.Data;
 using UnityEngine;
 using UnityEngine.Jobs;
 using Unity.Collections;
+using Unity.Jobs;
 
 namespace VRoxel.Navigation
 {
@@ -14,22 +15,73 @@ namespace VRoxel.Navigation
     public class NavAgentManager : MonoBehaviour
     {
         /// <summary>
-        /// The configurations for each type of NavAgent that should be managed
+        /// The configurations for each type of agent that will be managed
         /// </summary>
         public List<NavAgentConfiguration> configurations;
 
 
-
         /// <summary>
-        /// The current kinematic properties of each managed agent
+        /// The current kinematic properties for each agent
         /// </summary>
         protected NativeArray<AgentKinematics> m_agentKinematics;
 
         /// <summary>
-        /// The async transform access to each managed agents transform
+        /// The async transform access to each agent's transform
         /// </summary>
         protected TransformAccessArray m_transformAccess;
 
+        /// <summary>
+        /// The background job to move each agent in the scene
+        /// </summary>
+        protected JobHandle m_movingAgents;
+
+        /// <summary>
+        /// The background job to update each of the flow fields
+        /// </summary>
+        protected JobHandle m_updatingPathfinding;
+
+        //-------------------------------------------------
+        #region Public API
+
+        /// <summary>
+        /// Schedules a background job to move all agents using the given delta time
+        /// </summary>
+        public JobHandle MoveAgents(float dt, JobHandle dependsOn = default(JobHandle))
+        {
+            return m_movingAgents;
+        }
+
+        /// <summary>
+        /// Schedules a background job to update all flow fields to target the given goal
+        /// </summary>
+        public JobHandle UpdatePathfinding(Vector3Int goal, JobHandle dependsOn = default(JobHandle))
+        {
+            return m_updatingPathfinding;
+        }
+
+        /// <summary>
+        /// Creates a new TransformAccessArray to asynchronously
+        /// access each of the given transforms
+        /// </summary>
+        public void SetTransformAccess(Transform[] transforms)
+        {
+            m_transformAccess = new TransformAccessArray(transforms);
+        }
+
+        /// <summary>
+        /// Disposes all unmanaged memory from the agent manager
+        /// </summary>
+        public void Dispose()
+        {
+            if (m_agentKinematics.IsCreated)
+                m_agentKinematics.Dispose();
+
+            if (m_transformAccess.isCreated)
+                m_transformAccess.Dispose();
+        }
+
+        #endregion
+        //-------------------------------------------------
 
 
         //-------------------------------------------------
