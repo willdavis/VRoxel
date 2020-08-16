418ec7e9bd53f25167a7ace6d15647db78defd38
willdavis <1072649+willdavis@users.noreply.github.com>
2020-08-15 17:51:47 -0700

    #89 created a struct to hold each agents position and velocity

---
+++ Assets/VRoxel/Navigation/Agents/AgentKinematics.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Unity.Mathematics;
+
+namespace VRoxel.Navigation.Agents
+{
+    /// <summary>
+    /// Describes the current position and velocity of an agent in the scene
+    /// </summary>
+    [Serializable]
+    public struct AgentKinematics
+    {
+        public bool active;
+        public float3 position;
+        public float3 velocity;
+    }
+}
