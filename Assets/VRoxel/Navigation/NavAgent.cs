4f0922d6409949a0534518a99dd3f6d6316d8a2a
willdavis <1072649+willdavis@users.noreply.github.com>
2020-08-17 15:10:13 -0700

    #111 added agent config variable to the NavAgent class

--- Assets/VRoxel/Navigation/NavAgent.cs
+++ Assets/VRoxel/Navigation/NavAgent.cs
@@ -1,9 +1,21 @@
-﻿using UnityEngine;
+﻿using VRoxel.Navigation.Data;
+using UnityEngine;
 
 namespace VRoxel.Navigation
 {
+    /// <summary>
+    /// A component to provide an agent with navigational properties
+    /// </summary>
     public class NavAgent : MonoBehaviour
     {
-        public int index;
+        /// <summary>
+        /// The agents index in the background job arrays
+        /// </summary>
+        [HideInInspector] public int index;
+
+        /// <summary>
+        /// The configuration settings for this type of agent
+        /// </summary>
+        public NavAgentConfiguration configuration;
     }
 }
