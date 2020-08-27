10870d11acd65e3d06279473098e688af36b57d7
willdavis <1072649+willdavis@users.noreply.github.com>
2020-08-27 14:04:38 -0700

    #16 created BlockConfiguration scriptable object

---
+++ Assets/VRoxel/Core/Data/BlockConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace VRoxel.Core.Data
+{
+    /// <summary>
+    /// Data container for common block settings
+    /// </summary>
+    [CreateAssetMenu(fileName = "BlockConfiguration.asset", menuName = "VRoxel/Block Configuration", order = 1)]
+    public class BlockConfiguration : ScriptableObject
+    {
+        /// <summary>
+        /// The name of this block for display in the UI
+        /// </summary>
+        public new string name;
+
+        /// <summary>
+        /// A short description of this block for display in the UI
+        /// </summary>
+        [Multiline] public string description;
+
+
+        /// <summary>
+        /// Determines if this block needs a collision mesh
+        /// </summary>
+        [Header("Block Properties")]
+        public bool collidable;
+
+        /// <summary>
+        /// Determines if this block can be edited by the player
+        /// </summary>
+        public bool editable;
+
+
+        /// <summary>
+        /// The texture coordinates to use
+        /// for all sides of the block
+        /// </summary>
+        [Header("Texture Coordinates")]
+        public Vector2 texture;
+    }
+}
