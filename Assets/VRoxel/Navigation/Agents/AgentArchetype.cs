e2196c9158ab289a75e3f9efcbfd4c42acb7cdba
willdavis <1072649+willdavis@users.noreply.github.com>
2020-08-30 15:12:54 -0700

    #119 created AgentArchetype struct and scriptable objects

---
+++ Assets/VRoxel/Navigation/Agents/AgentArchetype.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System;
+
+namespace VRoxel.Navigation.Agents
+{
+    /// <summary>
+    /// Describes the static properties of an agent archetype
+    /// </summary>
+    [Serializable]
+    public struct AgentArchetype
+    {
+        /// <summary>
+        /// The voxel height for agents of this archetype
+        /// </summary>
+        [Tooltip("the voxel height of this archetype")]
+        public int height;
+
+        /// <summary>
+        /// The voxel collision radius for agents of this archetype
+        /// </summary>
+        [Tooltip("the voxel collision radius of this archetype")]
+        public float radius;
+    }
+}
---
+++ Assets/VRoxel/Navigation/Data/NavAgentArchetype.cs
@@ -0,0 +1,27 @@
+﻿using VRoxel.Navigation.Agents;
+using UnityEngine;
+
+namespace VRoxel.Navigation.Data
+{
+    /// <summary>
+    /// Data container for NavAgent settings
+    /// </summary>
+    [CreateAssetMenu(fileName = "NavAgentArchetype.asset", menuName = "VRoxel/Navigation/NavAgent Archetype", order = 1)]
+    public class NavAgentArchetype : ScriptableObject
+    {
+        /// <summary>
+        /// The name of this archetype for display in the UI
+        /// </summary>
+        public new string name;
+
+        /// <summary>
+        /// The description of this archetype for display in the UI
+        /// </summary>
+        [Multiline] public string description;
+
+        /// <summary>
+        /// The properties common to all agents of this archetype
+        /// </summary>
+        public AgentArchetype configuration;
+    }
+}
