f9efe0313a2ef22a27e6ac8fa0f2f294afe46f07
willdavis <1072649+willdavis@users.noreply.github.com>
2020-10-08 14:34:23 -0700

    #104 added job to modify chunks inside a user defined rectangle

---
+++ Assets/Editor/Tests/Core/Chunks/ModifyRectangleSpec.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+using VRoxel.Core.Chunks;
+using Unity.Mathematics;
+using Unity.Collections;
+using Unity.Jobs;
+
+namespace CoreChunksSpecs
+{
+    public class ModifyRectangleSpec
+    {
+        [Test]
+        public void UpdatesVoxelsInARectangle()
+        {
+            int3 offset = int3.zero;
+            int3 size = new int3(1, 2, 2);
+            int flatSize = size.x * size.y * size.z;
+
+            NativeArray<byte> voxels = new NativeArray<byte>(flatSize, Allocator.Persistent);
+
+            ModifyRectangle job = new ModifyRectangle()
+            {
+                block = 2,
+                chunkOffset = offset,
+                chunkSize = size,
+                voxels = voxels,
+                end = new int3(0,1,0),
+                start = new int3(0,0,0),
+            };
+            job.Schedule().Complete();
+
+            Assert.AreEqual(2, voxels[0]);
+            Assert.AreEqual(0, voxels[1]);
+            Assert.AreEqual(2, voxels[2]);
+            Assert.AreEqual(0, voxels[3]);
+
+            voxels.Dispose();
+        }
+
+        [Test]
+        public void SkipsOutOfBoundPositions()
+        {
+            int3 offset = int3.zero;
+            int3 size = new int3(1, 1, 1);
+            int flatSize = size.x * size.y * size.z;
+
+            NativeArray<byte> voxels = new NativeArray<byte>(flatSize, Allocator.Persistent);
+
+            ModifyRectangle job = new ModifyRectangle()
+            {
+                block = 2,
+                chunkOffset = offset,
+                chunkSize = size,
+                voxels = voxels,
+                end = new int3(0,1,0),
+                start = new int3(0,0,0),
+            };
+            job.Schedule().Complete();
+
+            Assert.AreEqual(2, voxels[0]);
+            Assert.DoesNotThrow(job.Execute);
+
+            voxels.Dispose();
+        }
+    }
+}
---
+++ Assets/VRoxel/Core/Chunks/ModifyRectangle.cs
@@ -0,0 +1,107 @@
+﻿using Unity.Collections;
+using Unity.Mathematics;
+using Unity.Burst;
+using Unity.Jobs;
+
+namespace VRoxel.Core.Chunks
+{
+    /// <summary>
+    /// Modifies the voxels in a chunk within the bounds of a rectangle
+    /// </summary>
+    [BurstCompile]
+    public struct ModifyRectangle : IJob
+    {
+        /// <summary>
+        /// All voxels inside the rectangle will be set to this block index
+        /// </summary>
+        public byte block;
+
+        /// <summary>
+        /// The global position of this chunk
+        /// </summary>
+        public int3 chunkOffset;
+
+        /// <summary>
+        /// The dimensions of this chunk
+        /// </summary>
+        public int3 chunkSize;
+
+        /// <summary>
+        /// The global position to start the rectangle
+        /// </summary>
+        public int3 start;
+
+        /// <summary>
+        /// The global position to end the rectangle
+        /// </summary>
+        public int3 end;
+
+        /// <summary>
+        /// The chunks voxel data that will be modified
+        /// </summary>
+        [WriteOnly] public NativeArray<byte> voxels;
+
+
+        /// <summary>
+        /// Iterate through the rectangle and update voxels in the chunk
+        /// </summary>
+        public void Execute()
+        {
+            int3 min = int3.zero;
+            int3 delta = int3.zero;
+            int3 localPos = int3.zero;
+            int3 globalPos = int3.zero;
+
+            // calculate min and delta of the rectangle so the
+            // start and end positions orientation will not matter
+            delta.x = math.abs(end.x - start.x) + 1;
+            delta.y = math.abs(end.y - start.y) + 1;
+            delta.z = math.abs(end.z - start.z) + 1;
+            
+            min.x = math.min(start.x, end.x);
+            min.y = math.min(start.y, end.y);
+            min.z = math.min(start.z, end.z);
+
+            // skip any point that is outside the chunk
+            // and update any voxel inside the rectangle
+            for (int x = min.x; x < min.x + delta.x; x++)
+            {
+                globalPos.x = x;
+                for (int z = min.z; z < min.z + delta.z; z++)
+                {
+                    globalPos.z = z;
+                    for (int y = min.y; y < min.y + delta.y; y++)
+                    {
+                        globalPos.y = y;
+
+                        if (OutOfChunk(globalPos))
+                            continue;
+
+                        localPos = globalPos - chunkOffset;
+                        voxels[Flatten(localPos)] = block;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Calculate an array index from an int3 (Vector3Int) point
+        /// </summary>
+        public int Flatten(int3 point)
+        {
+            /// A[x,y,z] = A[ x * height * depth + y * depth + z ]
+            return (point.x * chunkSize.y * chunkSize.z) + (point.y * chunkSize.z) + point.z;
+        }
+
+        /// <summary>
+        /// Test if a global position is outside the chunk
+        /// </summary>
+        public bool OutOfChunk(int3 point)
+        {
+            if (point.y < chunkOffset.y || point.y >= chunkOffset.y + chunkSize.y) { return true; }
+            if (point.z < chunkOffset.z || point.z >= chunkOffset.z + chunkSize.z) { return true; }
+            if (point.x < chunkOffset.x || point.x >= chunkOffset.x + chunkSize.x) { return true; }
+            return false;
+        }
+    }
+}
